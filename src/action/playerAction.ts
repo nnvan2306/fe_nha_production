cc84e5052b4f0f9fd0aba692d4f509d0882ff584
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-03-16 00:27:08 +0700

    player action

--- src/action/playerAction.ts
+++ src/action/playerAction.ts
@@ -1,7 +1,15 @@
 import { IPlayer } from "@/utils/interface";
 
 export const getPlayerAction =async():Promise<IPlayer>=>{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/get-player?page=1&pageSize=${process.env.NEXT_PUBLIC_PAGESIZE_PLAYER}`,{
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/get-player-active`,{
+        cache: "no-store",
+    })
+    const data = await res.json();
+    return data;
+}
+
+export const getOnePlayerAction = async(id:number):Promise<IPlayer>=>{
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/get-player-active?playerId=${id}`,{
         cache: "no-store",
     })
     const data = await res.json();
