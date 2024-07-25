d147fa36a040744a88d4bc248800bb9487411098
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-07-25 14:23:15 +0700

    update

--- src/app/(marketing)/home/booking/[id]/buyTicket/page.tsx
+++ src/app/(marketing)/home/booking/[id]/buyTicket/page.tsx
@@ -1,3 +1,39 @@
+// import { handleGetOneTicket } from "@/action/ticketAction";
+// import PageBookingDetail from "@/components/Booking/PageBookingDetail";
+// import { Suspense } from "react";
+
+// interface IParams {
+//     params: {
+//         id: number;
+//     };
+// }
+
+// async function HandleData({ id }: { id: number }) {
+//     const res = await handleGetOneTicket(id);
+
+//     if (res.errorCode === 0) {
+//         return (
+//             <PageBookingDetail
+//                 infoTicketStart={res.data}
+//                 totalPriceStart={res.data.price}
+//                 priceStart={res.data.price}
+//             />
+//         );
+//     }
+// }
+
+// export default async function PageTicket({ params: { id } }: IParams) {
+//     return (
+//         <div>
+//             <Suspense fallback={<div>Loading.....</div>}>
+//                 <div>
+//                     <HandleData id={id} />
+//                 </div>
+//             </Suspense>
+//         </div>
+//     );
+// }
+
 import { handleGetOneTicket } from "@/action/ticketAction";
 import PageBookingDetail from "@/components/Booking/PageBookingDetail";
 import { Suspense } from "react";
@@ -6,6 +42,12 @@ interface IParams {
     params: {
         id: number;
     };
+}
+
+interface TicketData {
+    id: number;
+    price: number;
+    // Các thuộc tính khác của ticketData nếu có
 }
 
 async function HandleData({ id }: { id: number }) {
@@ -19,6 +61,8 @@ async function HandleData({ id }: { id: number }) {
                 priceStart={res.data.price}
             />
         );
+    } else {
+        return <div>Error loading data</div>;
     }
 }
 
