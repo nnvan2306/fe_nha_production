1a5004fabbb25ca0f9963c90451b910cc88dd492
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-07-31 10:58:41 +0700

    update

--- src/action/commentAction.ts
+++ src/action/commentAction.ts
@@ -1,140 +1,225 @@
-"use server"
+"use server";
 
-import {  IComment, IListLimit, IRes } from "@/utils/interface";
+import { IComment, IListLimit, IRes } from "@/utils/interface";
 import { revalidateTag } from "next/cache";
 
-export const handleGetCommentAction = async({page , pageSize , matchId} : {page:number , pageSize : number , matchId: number}):Promise<IRes<IListLimit<IComment> >>=>{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/get-comment?page=${page}&pageSize=${pageSize}&matchId=${matchId}` , {
-        next : {tags :['getComment']}
-    });
+export const handleGetCommentAction = async ({
+    page,
+    pageSize,
+    matchId,
+}: {
+    page: number;
+    pageSize: number;
+    matchId: number;
+}): Promise<IRes<IListLimit<IComment>>> => {
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/get-comment?page=${page}&pageSize=${pageSize}&matchId=${matchId}`,
+        {
+            next: { tags: ["getComment"] },
+        }
+    );
     const data = await res.json();
     return data;
-}
+};
 
+export const handleCreateComment = async ({
+    content,
+    matchId,
+    userId,
+}: {
+    content: string;
+    matchId: number;
+    userId: number;
+}): Promise<IRes<[]>> => {
+    revalidateTag("getComment");
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/create-comment`,
+        {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
 
-
-
-export const handleCreateComment = async({content ,matchId, userId } : {content : string , matchId :number , userId:number}):Promise<IRes<[]>>=>{
-    revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/create-comment`,{
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        body: JSON.stringify({content : content , matchId : matchId , userId : userId}),
-        cache:"no-store",
-    });
-    const data =await res.json();
+            body: JSON.stringify({
+                content: content,
+                matchId: matchId,
+                userId: userId,
+            }),
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
     return data;
-}
+};
 
+export const handleLikeCommentAction = async ({
+    commentId,
+    userId,
+}: {
+    commentId: number;
+    userId: number;
+}): Promise<IRes<[]>> => {
+    // revalidateTag('getComment');
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/like-comment`,
+        {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+            },
 
-export const  handleLikeCommentAction = async({ commentId , userId   } : {commentId : number ,userId : number  }) : Promise<IRes<[]>>=>{
-    // revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/like-comment`,{
-        method: 'PATCH',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        body: JSON.stringify({commentId : commentId , userId : userId }),
-        cache:"no-store",
-    });
-    const data =await res.json();
+            body: JSON.stringify({ commentId: commentId, userId: userId }),
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
     return data;
-}
+};
 
+export const handleDislikeCommentAction = async ({
+    commentId,
+    userId,
+}: {
+    commentId: number;
+    userId: number;
+}): Promise<IRes<[]>> => {
+    // revalidateTag('getComment');
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/dislike-comment`,
+        {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+            },
 
-export const  handleDislikeCommentAction = async({commentId , userId   } : {commentId : number , userId:number  }) : Promise<IRes<[]>>=>{
-    // revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dislike-comment`,{
-        method: 'PATCH',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        body: JSON.stringify({commentId:commentId , userId:userId   }),
-        cache:"no-store",
-    });
-    const data =await res.json();
+            body: JSON.stringify({ commentId: commentId, userId: userId }),
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
     return data;
-}
+};
 
 //
-//************************************************* */
 //feedback
 
-export const handleLikeFeedbackAction = async({feedbackId , userId} : {feedbackId : number  , userId : number}) : Promise<IRes<[]>>=>{
-    // revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/like-feedback`,{
-        method: 'PATCH',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        body: JSON.stringify({feedbackId : feedbackId , userId : userId }),
-        cache:"no-store",
-    });
-    const data =await res.json();
+export const handleLikeFeedbackAction = async ({
+    feedbackId,
+    userId,
+}: {
+    feedbackId: number;
+    userId: number;
+}): Promise<IRes<[]>> => {
+    // revalidateTag('getComment');
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/like-feedback`,
+        {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+            },
+
+            body: JSON.stringify({ feedbackId: feedbackId, userId: userId }),
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
     return data;
-}
+};
 
-export const handleDislikeFeedbackAction = async({feedbackId , userId} : {feedbackId : number  , userId : number}) : Promise<IRes<[]>>=>{
-    // revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dislike-feedback`,{
-        method: 'PATCH',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        body: JSON.stringify({feedbackId : feedbackId , userId : userId }),
-        cache:"no-store",
-    });
-    const data =await res.json();
+export const handleDislikeFeedbackAction = async ({
+    feedbackId,
+    userId,
+}: {
+    feedbackId: number;
+    userId: number;
+}): Promise<IRes<[]>> => {
+    // revalidateTag('getComment');
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/dislike-feedback`,
+        {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+            },
+
+            body: JSON.stringify({ feedbackId: feedbackId, userId: userId }),
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
     return data;
-}
+};
 
-export const handleDeleteCommentAction = async ({commentId} : {commentId : number}) :Promise<IRes<[]>>=>{
-    // revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/delete-comment?commentId=${commentId}`,{
-        method: 'DELETE',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        // body: JSON.stringify({feedbackId : feedbackId , userId : userId }),
-        cache:"no-store",
-    });
-    const data =await res.json();
-    return data;   
-}
+export const handleDeleteCommentAction = async ({
+    commentId,
+}: {
+    commentId: number;
+}): Promise<IRes<[]>> => {
+    // revalidateTag('getComment');
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/delete-comment?commentId=${commentId}`,
+        {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json",
+            },
 
-export const handleCreateFeedbackAction = async ({content , commentId , userId} : {content:string , commentId : number , userId : number}) :Promise<IRes<[]>>=>{
-    revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/create-feedback`,{
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        body: JSON.stringify({content : content , commentId : commentId , userId : userId }),
-        cache:"no-store",
-    });
-    const data =await res.json();
-    return data; 
-}
+            // body: JSON.stringify({feedbackId : feedbackId , userId : userId }),
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
+    return data;
+};
 
-export const handleDeleteFeedbackAction = async ({feedbackId} : {feedbackId : number}) :Promise<IRes<[]>>=>{
-    // revalidateTag('getComment'); 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/delete-feedback?feedbackId=${feedbackId}`,{
-        method: 'DELETE',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        
-        cache:"no-store",
-    });
-    const data =await res.json();
-    return data;   
-}
+export const handleCreateFeedbackAction = async ({
+    content,
+    commentId,
+    userId,
+}: {
+    content: string;
+    commentId: number;
+    userId: number;
+}): Promise<IRes<[]>> => {
+    revalidateTag("getComment");
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/create-feedback`,
+        {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+
+            body: JSON.stringify({
+                content: content,
+                commentId: commentId,
+                userId: userId,
+            }),
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
+    return data;
+};
+
+export const handleDeleteFeedbackAction = async ({
+    feedbackId,
+}: {
+    feedbackId: number;
+}): Promise<IRes<[]>> => {
+    // revalidateTag('getComment');
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/delete-feedback?feedbackId=${feedbackId}`,
+        {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json",
+            },
+
+            cache: "no-store",
+        }
+    );
+    const data = await res.json();
+    return data;
+};
