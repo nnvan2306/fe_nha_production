54dd2d7a3f0606dbb8d79037c62d0dd8665007ab
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-08-08 19:04:59 +0700

    update

--- src/components/ControlAuth/ControlAuth.tsx
+++ src/components/ControlAuth/ControlAuth.tsx
@@ -24,7 +24,71 @@ export default function ControlAuth() {
 
     return (
         <div className=" flex justify-center items-center h-[100%] w-[100%]">
-            {isLogin ? (
+            <div className="flex justify-end items-center w-[100%]">
+                <Tooltip
+                    className="bg-[#fff] p-[0]"
+                    trigger="click"
+                    title={
+                        <div className="h-[100px] w-[100px] bg-[#fff] p-[5px] rounded-[5px]">
+                            {isLogin ? (
+                                <>
+                                    <button
+                                        className="w-[100%] h-[30px] border-none mt-[10px] rounded-[5px]"
+                                        onClick={handleLogout}
+                                    >
+                                        <i className="bi bi-box-arrow-left mr-[5px]"></i>{" "}
+                                        Logout
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <Link href={routes.login.url}>
+                                        <button className="w-[100%] h-[30px] border-none mt-[10px] rounded-[5px]">
+                                            Đăng Nhập
+                                        </button>
+                                    </Link>
+
+                                    <Link href={routes.register.url}>
+                                        <button className="w-[100%] h-[30px] border-none mt-[10px] rounded-[5px]">
+                                            Đăng Nhập
+                                        </button>
+                                    </Link>
+                                </>
+                            )}
+
+                            <button className="w-[100%] h-[30px] border-none mt-[10px] rounded-[5px]">
+                                <i className="bi bi-gear mr-[5px]"></i>
+                                settings
+                            </button>
+                        </div>
+                    }
+                >
+                    <div
+                        className={`${handlebackground(
+                            color
+                        )}  w-[40px] h-[40px] rounded-full flex justify-center items-center border-solid border-[1px] border-[#fff] mr-[20px] md:mr-[0]`}
+                    >
+                        {avatar ? (
+                            <Image
+                                alt="avatar"
+                                src={`${process.env.NEXT_PUBLIC_BASE_URL}${avatar}`}
+                                width={40}
+                                height={40}
+                                className="rounded-full"
+                            />
+                        ) : (
+                            <p className="text-[20px] font-[500] text-[#fff]">
+                                {nameUser.slice(0, 1).toUpperCase()}
+                            </p>
+                        )}
+                    </div>
+                </Tooltip>
+
+                <p className="text-[20px] ml-[20px] text-[#fff] mr-[40px] md:block hidden">
+                    {nameUser}
+                </p>
+            </div>
+            {/* {isLogin ? (
                 <>
                     <div className="flex justify-end items-center w-[100%]">
                         <Tooltip
@@ -44,6 +108,20 @@ export default function ControlAuth() {
                                         <i className="bi bi-gear mr-[5px]"></i>
                                         settings
                                     </button>
+
+                                    {isLogin ? (
+                                        <></>
+                                    ) : (
+                                        <>
+                                            <button className="w-[100%] h-[30px] border-none mt-[10px] rounded-[5px]">
+                                                Đăng Nhập
+                                            </button>
+
+                                            <button className="w-[100%] h-[30px] border-none mt-[10px] rounded-[5px]">
+                                                Đăng Nhập
+                                            </button>
+                                        </>
+                                    )}
                                 </div>
                             }
                         >
@@ -81,13 +159,13 @@ export default function ControlAuth() {
                         </button>
                     </Link>
 
-                    {/* <Link href={routes.register.url}>
+                    <Link href={routes.register.url}>
                         <button className=" bg-[#3F1052] w-[100px] h-[40px] mt-[20px] p-[10px] text-[#fff] border-[1px] border-[#fff] rounded-[5px] mx-[10px] hover:bg-[#fff] hover:text-[#000]">
                             Đăng Ký
                         </button>
-                    </Link> */}
+                    </Link>
                 </>
-            )}
+            )} */}
         </div>
     );
 }
